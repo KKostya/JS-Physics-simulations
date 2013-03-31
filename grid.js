c762f99503f450c8d68ca4f198500f9da4848894
kanishev <kkanishev@gmail.com>
2013-03-31 01:42:25 +0100

    reorganizing grid code

--- grid.js
+++ grid.js
@@ -1,60 +1,125 @@
-function Grid(svg,st)
-{
-    this.svg = svg.data([this]); // Attaching self to the d3.data
-    this.w   = svg.attr("width");
-    this.h   = svg.attr("height");
-    this.st  = st
+// ***************************************************
+// **************** Constructor **********************
+// ***************************************************
 
-    this.elems  = d3.range(0,w,this.st).map(function(d){return d3.range(0,h,this.st).map(function(x){return null;})});
+function Grid(id,width,height,step)
+{
+    this.st  = step;
+    this.w   = step * width;
+    this.h   = step * height;
+
+    this.svg = d3.select(id).append("svg")
+                            .attr("width", this.w)
+                            .attr("height",this.h)
+                            .data([this]); // Attaching self to the d3.data
+
+    // 2D array of elements, null initialized
+    this.elems = d3.range(0,this.w,this.st)
+                   .map(  
+                           function(d) 
+                           { 
+                               return d3.range(0,this.h,this.st)
+                                        .map( function(x){return null;} ) 
+                           }
+                       );
 
     this.boxes = this.svg.append("g");
     this.lines = this.svg.append("g");
 
     this.shbox  = null;
     this.shline = null;
-
     this.linemode = 0;
+    this.boxcount = 0;
 }
 
-Grid.prototype. SetRedLineMode = function() { this.linemode = 0; this.LineMode(); }
-Grid.prototype.SetBlueLineMode = function() { this.linemode = 1; this.LineMode(); }
+Grid.prototype.BoxIdx  = function(r) { return [Math.round(r[0]/this.st-0.5),Math.round(r[1]/this.st-0.5)];}
 
-Grid.prototype.BoxIdx  = function(r) { return r.map(function(d){return Math.round(d/this.st-0.5);});}
-
-// *--3--*---> x
-// |     |
-// 2     1
-// |     |
-// *--0--*
-// |
-// y
 Grid.prototype.LineIdx = function(r)                 
 { 
-    var dr = r.map(function(x){return x%this.st;}); 
-    var ret = 0;
-    if(dr[0] > dr[1]) ret +=1;                    
-    if((this.st-dr[0]) > dr[1]) ret +=2;               
-    return ret;
+    var dr = r.map(function(x){return x%this.st;});  // *--3--*---> x
+    var ret = 0;                                     // |     |
+    if(dr[0] > dr[1]) ret +=1;                       // 2     1
+    if((this.st-dr[0]) > dr[1]) ret +=2;             // |     |         
+    return ret;                                      // *--0--*
+}                                                    // |
+                                                     // y
+Grid.prototype.NeighbourIndex = function(idx,xgty,rxgty)
+{
+    var nidx = idx.slice(0); // "slice(0)" makes a copy of an array
+    if(xgty) if(rxgty) nidx[1]--; else  nidx[0]++;
+    else     if(rxgty) nidx[0]--; else  nidx[1]++;
+    return nidx;
 }
 
+// ***********************************************
+// ***************** Box Mode ********************
+// ***********************************************
+
+/////////////////////// Updaters ////////////////////////////
+// Note that in updaters "this" is a current DOM object
+// while "d" is the d3.data that was associated with it
+// namely -- the grid object 
+///////////////////////////////////////////////////////////
+Grid.prototype.ClickBox = function(d) 
+{
+    var idx = d.BoxIdx(d3.mouse(this));
+    d.shbox.attr("x",d.st*idx[0]).attr("y",d.st*idx[1]);
+    if(d.TestBox(idx)) d.RemoveBox(idx);
+    else d.AddBox(idx); 
+    
+}
+Grid.prototype.UpdateBox = function(d) 
+{
+    var idx = d.BoxIdx(d3.mouse(this));
+    d.shbox.attr("x",d.st*idx[0]).attr("y",d.st*idx[1]);
+    if(!d.TestBox(idx)) d.shbox.attr("fill","gray");
+}
+
+///////////////// Mode selector //////////////////
+Grid.prototype.BoxMode = function()
+{
+    this.svg
+        .on("mousemove",this.UpdateBox)   
+        .on("click",    this.ClickBox );  
+
+    if(!this.shbox)
+        this.shbox = this.svg.append("rect")
+                             .attr("id","shbox")
+                             .attr("x","0").attr("y","0")
+                             .attr("width",this.st).attr("height",this.st)
+                             .attr("fill","white");
+
+    if(this.shline) { this.shline.remove(); delete this.shline; }
+}
+
+
 //-!-!-!-!-!--!-!-! Do the bounds check, okay? xxxxxxxxxxxxx////////////////
-Grid.prototype.TestBox= function(r) { return this.elems[r[0]][r[1]] != null;} 
+Grid.prototype.TestBox       = function(r) { return this.elems[r[0]][r[1]] != null;} 
+
+Grid.prototype.TestNeighbors = function(r) 
+{ 
+    return this.elems[r[0]-1][r[1]] != null || 
+           this.elems[r[0]+1][r[1]] != null || 
+           this.elems[r[0]][r[1]+1] != null || 
+           this.elems[r[0]][r[1]-1] != null ;
+}
+
 
 Grid.prototype.AddBox = function(r)
 {
     var bx = this.boxes.append("rect")
-        .attr("x",r[0]*st).attr("y",r[1]*st)
-        .attr("width",st).attr("height",st)
+        .attr("x",r[0]*this.st).attr("y",r[1]*this.st)
+        .attr("width",this.st).attr("height",this.st)
         .attr("fill","black");
 
     this.elems[r[0]][r[1]]     = new Object;
     this.elems[r[0]][r[1]].box = bx;
 
-    var rn = r.slice(0);
-    if(this.TestBox(rn)) this.MakeLine(r,3); else this.RemoveLine(r, true, true);
-    if(this.TestBox(rn)) this.MakeLine(r,se,false); else this.RemoveLine(r,false,false);
-    if(this.TestBox(rn)) this.MakeLine(r,false, true); else this.RemoveLine(r,false, true);
-    if(this.TestBox(rn)) this.MakeLine(r, true,false); else this.RemoveLine(r, true,false);
+//    var rn = r.slice(0);
+//    if(this.TestBox(rn)) this.MakeLine(r, true, true); else this.RemoveLine(r, true, true);
+//    if(this.TestBox(rn)) this.MakeLine(r,false,false); else this.RemoveLine(r,false,false);
+//    if(this.TestBox(rn)) this.MakeLine(r,false, true); else this.RemoveLine(r,false, true);
+//    if(this.TestBox(rn)) this.MakeLine(r, true,false); else this.RemoveLine(r, true,false);
 }
 
 Grid.prototype.RemoveBox = function(r)
@@ -63,14 +128,65 @@ Grid.prototype.RemoveBox = function(r)
     delete this.elems[r[0]][r[1]];
 }
 
-///////////////////////////////////////// Liney part ////////////////////////////////////////////
+// ***********************************************
+// ***************** Line Mode *******************
+// ***********************************************
 
-Grid.prototype.NeighbourIndex = function(idx,xgty,rxgty)
+/////////////////////// Updaters ////////////////////////////
+// Note that in updaters "this" is a current DOM object
+// while "d" is the d3.data that was associated with it
+// namely -- the grid object 
+///////////////////////////////////////////////////////////
+
+Grid.prototype.ClickLine = function(d) 
 {
-    var nidx = idx.slice(0); // "slice(0)" makes a copy of an array
-    if(xgty) if(rxgty) nidx[1]--; else  nidx[0]++;
-    else     if(rxgty) nidx[0]--; else  nidx[1]++;
-    return nidx;
+    var idx = d.BoxIdx(d3.mouse(this));
+    var  dr = d3.mouse(this).map(function(x){return x%d.st;}); 
+
+    var  xgty = dr[0] > dr[1];                    
+    var rxgty = (d.st-dr[0]) > dr[1];               
+
+    var nidx = d.NeighbourIndex(idx,xgty,rxgty); 
+
+    // "!=" is a javascript way of xoring 
+    if(d.TestBox(nidx) != d.TestBox(idx)) d.MakeLine(idx,xgty,rxgty);
+}
+
+Grid.prototype.UpdateLine = function(d) 
+{
+    var idx = d.BoxIdx(d3.mouse(this));
+    var  dr = d3.mouse(this).map(function(x){return x%d.st;}); 
+    
+    var  xgty = dr[0] > dr[1];                    
+    var rxgty = (d.st-dr[0]) > dr[1];               
+                                                  
+    d.PositionLine(d.shline,idx,xgty,rxgty);
+    var nidx = d.NeighbourIndex(idx,xgty,rxgty); 
+
+    // "!=" is a javascript way of xoring 
+    if(d.TestBox(nidx) != d.TestBox(idx)) 
+        if(d.linemode == 0) d.shline.attr("stroke","red"); 
+                       else d.shline.attr("stroke","blue");
+    else d.shline.attr("stroke","gray"); 
+}
+
+///////////////// Mode selector //////////////////
+Grid.prototype. SetRedLineMode = function() { this.linemode = 0; this.LineMode(); }
+Grid.prototype.SetBlueLineMode = function() { this.linemode = 1; this.LineMode(); }
+Grid.prototype.LineMode = function()
+{
+    this.svg
+        .on("mousemove",this.UpdateLine)   
+        .on("click",    this.ClickLine );  
+
+    if(!this.shline)
+        this.shline = this.svg.append("line")
+                              .attr("id","shline")
+                              .attr("x1","0").attr("y1","0")
+                              .attr("x2",this.st) .attr("y2","0")
+                              .attr("stroke","grey");
+
+    if(this.shbox) { this.shbox.remove(); delete this.shbox; }
 }
 
 Grid.prototype.PositionLine = function(obj,idx,xgty,rxgty)
@@ -104,95 +220,7 @@ Grid.prototype.RemoveLine = function(idx,xgty,rxgty)
 }
 
 
-/////////////////////// Updaters ////////////////////////////
-// Note that in updaters "this" is a current DOM object
-// while "d" is the d3.data that was associated with it
-///////////////////////////////////////////////////////////
-
-// Boxxy
-Grid.prototype.ClickBox = function(d) 
-{
-    var idx = d.BoxIdx(d3.mouse(this));
-    d.shbox.attr("x",st*idx[0]).attr("y",st*idx[1]);
-    if(!d.TestBox(idx)) d.AddBox(idx); 
-    else d.RemoveBox(idx);
-}
-Grid.prototype.UpdateBox = function(d) 
-{
-    var idx = d.BoxIdx(d3.mouse(this));
-    d.shbox.attr("x",st*idx[0]).attr("y",st*idx[1]);
-    if(!d.TestBox(idx)) d.shbox.attr("fill","gray");
-}
-
-
-// Liney
-Grid.prototype.ClickLine = function(d) 
-{
-    var idx = d.BoxIdx(d3.mouse(this));
-    var  dr = d3.mouse(this).map(function(x){return x%this.st;}); 
-
-    var  xgty = dr[0] > dr[1];                    
-    var rxgty = (st-dr[0]) > dr[1];               
-
-    var nidx = d.NeighbourIndex(idx,xgty,rxgty); 
-
-    // "!=" is a javascript way of xoring 
-    if(d.TestBox(nidx) != d.TestBox(idx)) 
-        d.MakeLine(idx,xgty,rxgty);
-}
-
-Grid.prototype.UpdateLine = function(d) 
-{
-    var idx = d.BoxIdx(d3.mouse(this));
-    var  dr = d3.mouse(this).map(function(x){return x%this.st;}); // I have no idea why "this" here is a grid instance
-    
-    var  xgty = dr[0] > dr[1];                    
-    var rxgty = (st-dr[0]) > dr[1];               
-                                                  
-    d.PositionLine(d.shline,idx,xgty,rxgty);
-    var nidx = d.NeighbourIndex(idx,xgty,rxgty); 
-
-    // "!=" is a javascript way of xoring 
-    if(d.TestBox(nidx) != d.TestBox(idx)) 
-        if(d.linemode == 0) d.shline.attr("stroke","red"); 
-                       else d.shline.attr("stroke","blue");
-    else d.shline.attr("stroke","gray"); 
-}
 
 
 
 
-
-/////////////////////////////// Mode selectors //////////////////////////////////
-
-Grid.prototype.BoxMode = function()
-{
-    this.svg
-        .on("mousemove",this.UpdateBox)   
-        .on("click",    this.ClickBox );  
-
-    if(!this.shbox)
-        this.shbox = svg.append("rect")
-            .attr("id","shbox")
-            .attr("x","0").attr("y","0")
-            .attr("width",st).attr("height",st)
-            .attr("fill","white");
-
-    if(this.shline) { this.shline.remove(); delete this.shline; }
-}
-
-Grid.prototype.LineMode = function()
-{
-    this.svg
-        .on("mousemove",this.UpdateLine)   
-        .on("click",    this.ClickLine );  
-
-    if(!this.shline)
-        this.shline = svg.append("line")
-            .attr("id","shline")
-            .attr("x1","0").attr("y1","0")
-            .attr("x2",st) .attr("y2","0")
-            .attr("stroke","grey");
-
-    if(this.shbox) { this.shbox.remove(); delete this.shbox; }
-}
